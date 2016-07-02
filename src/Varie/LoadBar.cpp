9d370462549d792cb802befa284cbc5efc252a78
stefanobelloni <stefano.belloni4@gmail.com>
2016-07-02 19:20:31 +0200

    removed everything from gh-pages branch

--- src/Varie/LoadBar.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  LoadBar.cpp
-//  Ecoli_4.1.3
-//
-//  Created by Stefano Belloni on 20/10/15.
-//  Copyright © 2015 Heidelberg Universitaet. All rights reserved.
-//
-
-#include "LoadBar.hpp"
